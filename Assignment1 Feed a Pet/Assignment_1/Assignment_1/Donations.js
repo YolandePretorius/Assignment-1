a1e7cefe3cd28ff6f66e0c05e1403396e030265e
Yolande <yolande.y.kok@gmail.com>
2020-12-03 20:45:27 +1300

    03/12/2020 Donatetion calculated

---
+++ Assignment1 Feed a Pet/Assignment_1/Assignment_1/Donations.js
@@ -0,0 +1,19 @@
+﻿
+var totAmountDonated = 2000;
+
+function myFunctionDonate(){
+    var amountDonated = document.getElementById("donationAmount").value;
+    checkAmount = parseFloat(amountDonated);
+
+    //alert(checkAmount * 2)
+
+    if (checkAmount < 0) {
+        alert("amount shoud be more than 0");
+    } else {
+        totAmountDonated = totAmountDonated + checkAmount;
+        alert("Thanks for your donation of $ " + checkAmount);
+        alert("Total anount donated so far is $ " + totAmountDonated);
+    }
+    return totAmountDonated;
+}
+
